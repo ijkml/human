69a292ba1427c0c8ce8d6269472ddee39be6c4c3
ML <me.mlaure@gmail.com>
2023-06-06 00:46:18 +0100

    add hero

---
+++ components/Home/Hero.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section id="home">
+    <h1>Mach Aaron</h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+#home {
+  min-height: calc(100vh - $nav-height);
+
+  @apply bg-ml-1 p-16;
+}
+
+h1 {
+  @apply font-light text-center tracking-tighter text-10 lg:text-30 text-ml-8/100;
+}
+</style>
