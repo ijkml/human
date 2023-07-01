fe5aebbec3013f69fce49c38d2cf00ab475d04c2
ML <me.mlaure@gmail.com>
2023-07-01 20:10:18 +0100

    add avatar

---
+++ components/Avatar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Avatar from '@/assets/avatar.svg?component';
+</script>
+
+<template>
+  <Avatar class="avatar" height="250" />
+</template>
+
+<style scoped lang="scss">
+:where(.avatar) {
+  @apply fill-ml-6/100 transition-all-350;
+
+  :deep() {
+    path {
+      @apply transition-inherit;
+    }
+  }
+
+  :deep() {
+    path:hover + .eyes,
+    .eyes:hover {
+      @apply fill-ml-0/100;
+    }
+  }
+}
+</style>
