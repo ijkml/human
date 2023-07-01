5a43877b70ee105234819af4377afefcdf45fa5d
ML <me.mlaure@gmail.com>
2023-07-01 20:10:07 +0100

    update projects

---
+++ components/OSS.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="oss-sect">
+    <div class="head-wrap">
+      <h2 class="screamer">OSS</h2>
+      <div class="reverb">
+        <p>Some open-source projects I have contributed to...</p>
+        <code>chore(docs): fix typo</code>
+      </div>
+    </div>
+
+    <div class="project-grid"></div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.oss-sect {
+  @apply bg-ml-8/100 text-ml-3/100 p-16;
+}
+.head-wrap {
+  @apply grid gap-3;
+}
+
+.screamer {
+  @apply text-ml-2/100;
+}
+
+code {
+  @apply text-3.4 font-mono bg-ml-6
+    py-0.25em px-0.4em inline-block;
+}
+
+.reverb {
+  @apply space-y-2 px-1 md:(text-4.2);
+}
+
+.project-grid {
+  @apply mt-8 grid gap-8;
+}
+</style>
---
+++ components/ProjectCard.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="project-card">
+    <div class="pc-inner">
+      <div class="pc-details">
+        <h3 class="title">PennyPay</h3>
+
+        <div class="sub-sect">
+          <div class="sub-head">Roles</div>
+          <div>Designer, Developer</div>
+        </div>
+
+        <div class="sub-sect">
+          <div class="sub-head">Project</div>
+          <div>
+            He stared at the clinic, Molly took him to the simple Chinese hollow
+            points Shin had sold him.
+          </div>
+        </div>
+
+        <div class="sub-sect">
+          <div class="sub-head">Tech</div>
+          <div class="tech">Vue, Nuxt, Headless UI</div>
+        </div>
+
+        <div>
+          <ZeButton text="View Demo" small />
+        </div>
+      </div>
+
+      <div class="pc-img">
+        <img src="@img/og2.png" alt="ALT" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.project-card {
+  @apply bg-ml-8/100 rd-2 p-2
+    sm:(p-6 rd-3) md:(p-8);
+
+  &:where(:hover, :focus-within) {
+    .pc-img {
+      @apply saturate-85;
+    }
+  }
+}
+
+.pc-inner {
+  @apply grid items-start gap-8 auto-flow-dense
+    sm:(grid-cols-5) md:(grid-cols-3);
+}
+
+.pc-img {
+  @apply self-center row-end--1 transition-350 saturate-35
+    sm:(row-end-auto col-span-3) md:(col-span-2) lg:(p-2);
+
+  img {
+    @apply w-full h-full object-(cover center)
+      rd-1 aspect-1.6 sm:(rd-2);
+  }
+}
+
+.pc-details {
+  @apply px-4 mb-5 grid gap-4 sm:(px-0 col-span-2)
+    md:(col-span-1) lg:(pr-4 py-2);
+}
+
+.title {
+  @apply text-(6.5/[1.1] ml-2/90) sm:(text-7.5) md:(text-8.5);
+}
+
+.sub-sect {
+  @apply p-1 text-base max-w-50ch;
+}
+
+.sub-head {
+  @apply text-3.3/[1.5] font-mono text-ml-3/100
+    border-(b-1 ml-3/35) min-w-25 inline-block
+      tracking-wide uppercase mb-2.5 ml--1 px-1;
+}
+
+.tech {
+  @apply font-mono uppercase text-3.3/[1.5] tracking-wide;
+}
+</style>
