6ea5cd58b10a09e1140763f3ad56a6e6978a05be
ML <me.mlaure@gmail.com>
2023-07-22 00:59:28 +0100

    update nuxt umami (and config)

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   umami: {
     autoTrack: true,
     host: 'https://analytics.umami.is/',
-    id: 'c1ebff10-5639-46d7-83e3-a901b81b2c6f',
+    // id: 'c1ebff10-5639-46d7-83e3-a901b81b2c6f',
     version: 2,
     useDirective: true,
     // ignoreLocalhost: true,
