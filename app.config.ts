c4e33392b6326514423fefdaf6977c42235bf448
ML <me.mlaure@gmail.com>
2023-07-22 00:23:16 +0100

    set up umami

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,11 @@
 export default defineAppConfig({
   umami: {
     autoTrack: true,
-    // debug: true,
+    host: 'https://analytics.umami.is/',
+    id: 'c1ebff10-5639-46d7-83e3-a901b81b2c6f',
+    version: 2,
+    useDirective: true,
+    // ignoreLocalhost: true,
+    debug: true,
   },
 });
