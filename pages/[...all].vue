5ac3ed92eb27c0c26f2e4917898ed281c5a9e822
ML <me.mlaure@gmail.com>
2023-09-05 23:30:59 +0100

    update 404 page

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-const router = useRouter();
+useHead({
+  title: '404 Not Found',
+  meta: [{ name: 'description', content: 'This page does not exist.' }],
+});
+
+const props = {
+  title: 'Bro<span aria-hidden="true">·_·</span>ken',
+  paragraph:
+    'Congratulations, you found a broken link! <br /> If this is not what you were hoping to find, please give me a shout.',
+
+  links: [
+    { text: 'Go Home', link: '/' },
+    { text: 'FIX NOW!', link: '/contact' },
+  ],
+};
 </script>
 
 <template>
-  <main class="px-4 py-10 text-center text-teal-7 dark:text-gray-2">
-    <div class="text-4xl">
-      <div class="i-carbon-warning inline-block" />
-    </div>
-    <div>Not found</div>
-    <div>
-      <button class="btn text-sm m-(3 t8)" @click="router.back()">Back</button>
-    </div>
-  </main>
+  <NotFound v-bind="props" />
 </template>
