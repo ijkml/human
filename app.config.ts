95f5e2cfdc053cec89067760798f2c48553613e5
ML <me.mlaure@gmail.com>
2023-09-13 00:00:11 +0100

    misc

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   umami: {
-    autoTrack: true,
+    autoTrack: !true,
     host: 'https://analytics.umami.is/',
     // id: 'c1ebff10-5639-46d7-83e3-a901b81b2c6f',
     version: 2,
