de7fbfca3e568668303f045744e13d04573ec1d3
ML <me.mlaure@gmail.com>
2023-09-19 00:29:58 +0100

    posts: misc

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -6,8 +6,8 @@
 
 <style lang="scss" scoped>
 blockquote {
-  @apply my-6 py-2 b-l-(4 solid ml-5/60) text-ml-3/90
-    grid gap-3 max-w-50ch pl-4 ml--5 ps-6;
+  @apply my-6 py-2 b-l-(4 solid ml-5/60) text-ml-3/100
+    grid gap-3 max-w-50ch pl-4 ml--5 ps-5;
 
   quotes: none;
 
@@ -17,7 +17,7 @@ blockquote {
     }
 
     > figcaption {
-      @apply text-;
+      @apply font-semibold;
 
       &::before {
         @apply content-['—'] mr-1;
