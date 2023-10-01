1304bc539838e047a6b493b9e8b5cf49c589a9ff
ML <me.mlaure@gmail.com>
2023-10-01 20:06:30 +0100

    update umami info

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,6 @@
 export default defineAppConfig({
   umami: {
-    autoTrack: !true,
-    host: 'https://analytics.umami.is/',
-    // id: 'c1ebff10-5639-46d7-83e3-a901b81b2c6f',
     version: 2,
-    useDirective: true,
-    // ignoreLocalhost: true,
-    // debug: true,
+    domains: ['ijkml.vercel.app'],
   },
 });
