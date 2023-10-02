5e81b93a7029d79c4d07e959fb7a5ec95252da9c
ML <me.mlaure@gmail.com>
2023-10-02 02:41:55 +0100

    improve ux

--- components/Work/Featured.vue
+++ components/Work/Featured.vue
@@ -2,11 +2,18 @@
 import type { FeaturedCardProps } from '@data/work';
 import { wip } from '@data/work';
 
-const { data } = await useAsyncData('home', () => {
+const { data, pending } = await useAsyncData('work-projects', () => {
   return queryContent('work').only(['id', 'name']).find();
+}, {
+  lazy: true,
 });
 
-const featuredCards = [...((data.value ?? []) as unknown as FeaturedCardProps[]), ...wip];
+const featuredCards = [
+// const featuredCards = computed(() => [
+  ...((data.value ?? []) as unknown as FeaturedCardProps[]),
+  ...wip,
+// ]);
+];
 </script>
 
 <template>
@@ -26,13 +33,49 @@ const featuredCards = [...((data.value ?? []) as unknown as FeaturedCardProps[])
     </div>
 
     <div class="project-grid">
-      <WorkCard v-for="pr in featuredCards" :key="pr.id" v-bind="pr" />
+      <template v-if="pending">
+        <div v-for="i in 6" :key="i" role="status" class="project-card skeleton">
+          <UnoIcon class="i-carbon-image icon" />
+          <span class="sr-only" aria-hidden="true">Loading...</span>
+        </div>
+      </template>
+      <template v-else>
+        <WorkCard v-for="pr in featuredCards" :key="pr.id" v-bind="pr" />
+      </template>
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+@keyframes pulse {
+  0%,
+  100% {
+    opacity: 0.9;
+  }
+
+  50% {
+    opacity: 0.4;
+  }
+}
+
 .project-grid {
   @apply mt-8 grid gap-8 md:grid-cols-2;
+}
+
+:deep() {
+  .project-card {
+    @apply h-60 ss500:(aspect-2 h-auto);
+  }
+}
+
+.skeleton {
+  @apply flex items-center justify-center rd-lg
+    bg-ml-9/100;
+
+  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+
+  .icon {
+    @apply w-10 h-10 text-ml-4/75;
+  }
 }
 </style>
