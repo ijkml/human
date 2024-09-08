bbbb2b0a1ea0d6f49f632d462b0a598d15edd4e0
ML <me.mlaure@gmail.com>
2024-09-08 01:40:53 +0100

    exp: umami proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   ],
   umami: {
     domains: ['ijkml.dev', 'localhost'],
-    proxy: 'cloak',
+    proxy: 'direct',
   },
   site: {
     url: 'https://ijkml.dev',
